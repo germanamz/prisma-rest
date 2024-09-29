90c3c099f574d03dea7f8162755a5a083a264dc9
German Meza <13009536+germanamz@users.noreply.github.com>
2024-09-28 23:58:12 -0600

    zod-helpers-tests (#15)
    * added zod module helpers tests
    
    * added test task to turbo
    
    * added test to checks
    
    * ci tests need to build packages

--- packages/zod/src/helpers/is-prisma-scalar.ts
+++
@@ -1,3 +0,0 @@
-import { PRISMA_SCALAR, PrismaScalar } from '../constants/prisma-scalars';
-
-export const isPrismaScalar = (type: string): type is PrismaScalar => type in PRISMA_SCALAR;
--- packages/zod/src/helpers/prisma-to-ts-scalar.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaScalar } from '../constants/prisma-scalars';
-import { PRISMA_TS_SCALAR } from '../constants/prisma-ts-scalar';
-
-export const prismaToTsScalar = (type: PrismaScalar) => PRISMA_TS_SCALAR[type];

d5183cdd6004f10b9f7211d432cee54f691fafb6
German Meza <13009536+germanamz@users.noreply.github.com>
2024-11-21 11:12:36 -0600

    zod generate datamodel enums with marshal enums (#22)

--- packages/zod/src/generators/datamodel/generate-datamodel-enums.ts
+++ packages/zod/src/generators/datamodel/generate-datamodel-enums.ts
@@ -1,20 +1,13 @@
-import { Project } from 'ts-morph';
-import { DMMF } from '@prisma/generator-helper';
-import { ImportQueue, namespaceHandler, Registry } from '@germanamz/prisma-rest-toolbox';
+import {
+  GeneratorContext, namespaceHandler,
+} from '@germanamz/prisma-rest-toolbox';
 import { generateDatamodelEnum } from './generate-datamodel-enum';
 
-export type GenerateDatamodelEnumsOptions = {
-  dir: string;
-  project: Project;
-  registry: Registry;
-  importQueue: ImportQueue;
-  dmmf: DMMF.Document;
-};
+export type GenerateDatamodelEnumsOptions = GeneratorContext;
 
-// TODO: Use MarshalDocument
 export const generateDatamodelEnums = (options: GenerateDatamodelEnumsOptions) => namespaceHandler({
   ...options,
-  items: options.dmmf.datamodel.enums,
+  items: options.marshalDocument.enums,
   handler: (item) => generateDatamodelEnum({
     ...options,
     item,
